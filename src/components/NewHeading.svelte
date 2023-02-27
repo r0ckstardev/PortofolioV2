57e4c740dfceb1c41198b140f24b60efa585f2af
14x0 <hateisrichh@gmail.com>
2023-02-28 00:13:08 +0400

    fix status + listening + fonts and shit blog (soon)

--- src/components/NewHeading.svelte
+++ src/components/NewHeading.svelte
@@ -1,5 +1,47 @@
 <script>
     import Icon from '@iconify/svelte';
+    import { onMount } from 'svelte';
+let presence;
+let data;
+onMount(async () => {
+  const { useLanyard } = await import("svelte-lanyard");
+  data = useLanyard('892107677608599562');
+
+});
+
+  
+  let cordStatus = 'Loading'
+  let badgeColor = ''
+   $: artistName = $data?.spotify.artist
+   $: songName =   $data?.spotify.song
+   $: albumName =  $data?.spotify.album
+
+  let isListening = false
+  
+$: {
+    if($data?.spotify) {
+        isListening = true
+    } else {
+        isListening = false
+    }
+}
+
+$: {
+if($data?.discord_status === 'dnd') {
+    cordStatus = "Do not disturb"
+    badgeColor = '#ED4245'
+  } else if($data?.discord_status === 'idle') {
+    cordStatus = "Idle"
+    badgeColor = '#FEE75C'
+  } else if($data?.discord_status === 'online') {
+    cordStatus = "Online"
+    badgeColor = '#57F287'
+  }  else if($data?.discord_status === 'invisible') {
+    cordStatus = "Offline"
+    badgeColor = 'grey'
+  }
+}
+
 </script>
 
 <div class="flex px-[30vw] ml-[-3vh] mt-[4vh]">
@@ -7,8 +49,10 @@
     <div class="text-[#fff] ml-[3.5vh] mt-[2vh]">
         <p class="text-[#fff] text-[2rem] font-semibold">nate</p>
         <p class="text-[#fff] font-[550]">Software Developer | Computer Scientist</p>
-        <div class="flex align-middle items-center mt-[1vh] h-[2vh]"><Icon icon="carbon:dot-mark" width="32" height="32" /> <p class="text-[#a8a8a8] font-[550]">Offline</p></div>
-        <div class="flex align-middle items-center ml-[.4vh] mt-[1vh] h-[2vh]"><Icon icon="mdi:spotify" width="22" height="22" /> <p class="text-[#a8a8a8] font-[550] ml-[.5vh]">Currently Not Listening to anything</p></div>
+        <div class="flex align-middle items-center mt-[1vh] h-[2vh]"><Icon icon="carbon:dot-mark" width="32" height="32" color={badgeColor} /> <p class="text-[#a8a8a8] font-[550]">{cordStatus}</p></div>
+        <div class="flex align-middle items-center ml-[.4vh] mt-[1vh] h-[2vh]"><Icon icon="mdi:spotify" width="22" height="22" /> <p class="text-[#a8a8a8] font-[550] ml-[.5vh]">
+            {isListening ? `Listening to ${songName} by ${artistName} on ${albumName}` : "currently not listening to anything"}
+        </p></div>
         <div class="flex flex-row mt-[1.2vh]">
             <!-- <img class="" src="./../lib/github.svg" alt="github">
             <img class="mr-[1vh] ml-[1vh]" src="./../lib/telegram.svg" alt="telegram">
