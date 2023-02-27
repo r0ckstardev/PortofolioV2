23808df76bab834696530a7145e708b4df4f0ac9
14x0 <hateisrichh@gmail.com>
2023-02-28 00:13:37 +0400

    LOL testing page stuck

--- src/routes/hello/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import { useLanyard } from "sk-lanyard";
-        const presence = useLanyard({ method: 'ws', id: '892107677608599562' });
-</script>
-<code class="text-[#fff]">{JSON.stringify($presence ?? {}, null, 2)}</code>
