22b1fd0a32e7b6c5116d9cae6e32d1948ff014a3
r0ckstardev <110203517+r0ckstardev@users.noreply.github.com>
2023-03-09 23:55:53 +0400

    Update Heading.svelte

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -14,7 +14,7 @@
             class="text-xl mt-[1vh] font-semibold py-[.7vh] shadow-lg rounded-md bg-[#8ca4fc] px-[3vh] mr-[6vh]">Work With me</button>
     </div>
     <div class="px-[6vh] mr-[25vh] py-[8vh] float-right">
-        <img class="rounded-full" src="https://api.lanyard.rest/892107677608599562.png?size=1024" alt="">
+        <img class="rounded-full" src="https://api.lanyard.rest/892107677608599562.png" alt="">
         <p class="text-[#fff]"></p>
     </div>
 </div>
